76ede875301fa798d2342f6a53930615c09ce9a5
wallacesintra <wallvxwallace962@gmail.com>
2023-11-29 01:01:22 +0300

    the re-rendering issue fixed

--- src/screens/Search.js
+++ src/screens/Search.js
@@ -4,47 +4,48 @@ import { Feather } from '@expo/vector-icons';
 import CityDetails from "../components/CityDetails";
 import { WEATHER_API_KEY } from '@env'
 import { weatherType } from "../utilities/weatherType";
+import SearchLoad from "../components/Searchload";
 
 
 const Search = () => {
     const [cityName, setCityName] = useState('')
     const [cityData, setCityData] = useState([])
-    const [loading, setLoading] = useState(true)
+    const [error, SetError] = useState(null)
+    const [loading, setLoading] = useState(false)
 
     const fetchCityData = async () => {
         try {
-            if (cityName !== ''){
-                const res = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${WEATHER_API_KEY}&units=metric`)
-                const data = await res.json()
-                setCityData(data)
-            }   
+            const res = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${WEATHER_API_KEY}&units=metric`)
+            const data = await res.json()
+            setCityData(data)
+            setLoading(true)
+
         } catch (err) {
+            SetError(err)
             console.log(err)
         }
     }
 
     useEffect(() => {
-        ;(async () => {
-            await fetchCityData()    
+        ;(async() => {
+            await fetchCityData()
         })()
-    }, [cityName])
-    
+    },[cityName, error])
+
     if (cityData.list)
-    {
-        setLoading(false)
-    }
-    if (cityData)
     {
         console.log(Object.keys(cityData))
         console.log(cityData.cod)
-        console.log(cityData.message)
+        
     }
     
     const {list, city} = cityData
     return (
         <SafeAreaView style={styles.container}>
             <Text style={styles.title}>Search location</Text>
-            <Text style={styles.txt}>Search the area or city that you want to know the detailed weather info at this time</Text>
+            <Text style={styles.txt}>
+                Search the area or city that you want to 
+            know the detailed weather info at this time</Text>
 
             <View style={styles.txtInputWrapper}>
                 <Feather 
@@ -57,10 +58,10 @@ const Search = () => {
                     placeholder= {'Search'}
                     placeholderTextColor = 'white'
                     cursorColor = 'white' 
-                    onChangeText={newCity => setCityName(newCity)}
-                    // onChange={onSearchCity}
+                    onChange={(newCity) => setCityName(newCity)}
                     value={cityName}
-                    autoCapitalize='words'
+                    
+                    // autoCapitalize='words'
                 />                
             </View>
             {loading?
@@ -73,9 +74,7 @@ const Search = () => {
                     wind_speed = {list[0].wind.speed}
                     humidity = {list[0].main.humidity}
                 />:
-                <View>
-                    <Text>Search a city</Text>
-                </View>
+                <SearchLoad/>
             }
         </SafeAreaView>
     )
